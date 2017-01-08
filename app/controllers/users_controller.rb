bb9de79987063c597351baf1dbae382639f52281
awanderlyss <alyssa.felix7@gmail.com>
2017-01-08 00:11:04 -0500

    Completed layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   def ownerships
-    @ownerships = Ownership.where(user: current_user)
+    @ownerships = Ownership.where(user: current_user).paginate(:page => params[:page], :per_page => 6)
   end
 end
