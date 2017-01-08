358f71cbc727395cf7699be2823f9401a50479df
awanderlyss <alyssa.felix7@gmail.com>
2017-01-08 00:39:52 -0500

    Update footer w/ game genres

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def ownerships
+    @games = Game.all
     @ownerships = Ownership.where(user: current_user).paginate(:page => params[:page], :per_page => 6)
   end
 end
