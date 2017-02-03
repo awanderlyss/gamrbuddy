0a9f97c2501f5d026b424f6883747888398e8a84
awanderlyss <alyssa.felix7@gmail.com>
2017-02-03 13:20:33 -0500

    Yikes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,21 +1,4 @@
 # http://stackoverflow.com/questions/17767449/rails-4-0-with-devise-nested-attributes-unpermited-parameters
 class Users::RegistrationsController < Devise::RegistrationsController
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  # GET /users/sign_up
-  def new
-    # Override Devise default behaviour and create a profile as well
-    build_resource({})
-    resource.build_profile
-    respond_with self.resource
-  end
-
-  private
-
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u|
-        u.permit(:email, :password, :password_confirmation, :profile_attributes => [:gamertag, :img_url, :desc] )
-      }
-    end
 end
