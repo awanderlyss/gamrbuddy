e4fb20e3da5a07c636ebf98b6c73009defc5942b
awanderlyss <alyssa.felix7@gmail.com>
2017-02-03 13:37:48 -0500

    Revert back to init profile, no connections

--- app/controllers/users/registrations_controller.rb
+++
@@ -1,4 +0,0 @@
-# http://stackoverflow.com/questions/17767449/rails-4-0-with-devise-nested-attributes-unpermited-parameters
-class Users::RegistrationsController < Devise::RegistrationsController
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Users Routes
-  devise_for :users, controllers: {registrations: 'users/registrations'}
+  devise_for :users
 
   resources :users do
     member do
