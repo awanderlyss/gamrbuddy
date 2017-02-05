05a4fb8052b5cd59cf28f8236552f1a9bd4c6881
awanderlyss <alyssa.felix7@gmail.com>
2017-02-05 02:22:29 -0500

    User saves but profile isnt displaying

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,11 @@
 # http://stackoverflow.com/questions/3546289/override-devise-registrations-controller
 
 class RegistrationsController < Devise::RegistrationsController
+  def new
+    build_resource({})
+    self.resource.profile = Profile.new
+    respond_with self.resource
+  end
 
 
 end
