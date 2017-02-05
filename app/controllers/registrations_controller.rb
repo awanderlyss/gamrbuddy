d650e0fc5292f2ff1537ac3a9c8ce400a5e4ba55
awanderlyss <alyssa.felix7@gmail.com>
2017-02-05 05:22:20 -0500

    User Profile created, update data is saved & is displayed in edit view

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,53 @@
 # http://stackoverflow.com/questions/3546289/override-devise-registrations-controller
 
 class RegistrationsController < Devise::RegistrationsController
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
   def new
     build_resource({})
-    self.resource.profile = Profile.new
+    #self.resource.build_profile
+    #self.resource.profile = Profile.new
     respond_with self.resource
   end
 
+  def create
+
+    build_resource(sign_up_params)
+
+    resource.save
+
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+
+  def edit
+    super
+  end
+
+  private
+
+    def configure_permitted_parameters
+       devise_parameter_sanitizer.permit(:sign_up) { |u|
+          u.permit(:email, :password, :password_confirmation, profile_attributes: [:gamertag, :img_url, :desc])
+       }
+       devise_parameter_sanitizer.permit(:account_update) { |u|
+          u.permit(:email, :password, :password_confirmation, :current_password, profile_attributes: [:gamertag, :img_url, :desc])
+       }
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   #Associations
-  has_one :profile, dependent: :destroy
+  has_one :profile, dependent: :destroy, autosave: true
   has_many :ownerships
   has_many :games, through: :ownerships, dependent: :destroy
 
   accepts_nested_attributes_for :profile
-
+  before_create :build_profile
 end
